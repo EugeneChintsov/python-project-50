47526c7a2869e8fa73ecb9350651e75a38e02ff6
Eugene Chintsov <tr.e.chintsov@gmail.com>
2025-04-21 17:18:03 +0700

    Update: 	Fix 	modified:   gendiff/__init__.py 	modified:   gendiff/main.py

--- gendiff/main.py
+++ gendiff/main.py
@@ -1,11 +1,15 @@
-from typing import Any
+from typing import Any, Optional
 
 from gendiff.formatters.formatter import apply_formatter
 from gendiff.modules.diff_building import get_diff
 from gendiff.modules.file_parsing import get_data
 
 
-def generate_diff(first_file: str, second_file: str, format: str) -> Any:
+def generate_diff(
+        first_file: str,
+        second_file: str,
+        format: Optional[str] = "stylish",
+    ) -> Any:
     """
      Generates a difference report between two configuration files.
     """
